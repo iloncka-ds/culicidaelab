6dfb50bb3cd1d53722462da91b55450957f2e0c7
Ilona Kovaleva <iloncka.ds@gmail.com>
2025-10-11 18:37:18 +0300

    deps: bump version

--- culicidaelab/predictors/backends/__init__.py
+++ culicidaelab/predictors/backends/__init__.py
@@ -1,13 +1,1 @@
 """This package contains the various backend implementations for the predictors."""
-
-from .classifier._fastai import ClassifierFastAIBackend
-from .classifier._onnx import ClassifierONNXBackend
-from .detector._yolo import DetectorYOLOBackend
-from .segmenter._sam import SegmenterSAMBackend
-
-__all__ = [
-    "ClassifierFastAIBackend",
-    "ClassifierONNXBackend",
-    "DetectorYOLOBackend",
-    "SegmenterSAMBackend",
-]
